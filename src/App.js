4508607a915376f12a48d714450fb27953d23dba
Kylie Stewart <kylie.stewart@me.com>
2017-11-09 16:59:29 -0700

    Bare bones design

--- src/App.js
+++ src/App.js
@@ -4,14 +4,36 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app-container">
+        <header className="header">
+          <a href="">ABOUT</a>
+          <a href="">PROJECTS</a>
+          <a href="">CONTACT</a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="body">
+          <h1 className="title">Kylie Stewart</h1>
+          <div className="welcome">
+            <div className="welcome-div">
+              <p>More about me</p></div>
+            <div className="welcome-div">
+              <p>More about me</p></div>
+            <div className="welcome-div">
+              <p>More about me</p></div>
+          </div>
+
+          <div className="welcome">
+            <div className="welcome-div">
+              <p>More about me</p></div>
+            <div className="welcome-div">
+              <p>More about me</p></div>
+            <div className="welcome-div">
+              <p>More about me</p></div>
+          </div>
+          <div className="space"></div>
+        </div>
+        <footer className="footer">
+          Made with ❤️ in Phoenix. - view source
+        </footer>
       </div>
     );
   }
