da4a2afd561631f4ca2c6755f2a2993466c336cd
Kylie Stewart <kylie.stewart@me.com>
2017-11-09 19:26:51 -0700

    content containers are responsive

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ class App extends Component {
     return (
       <div className="app-container">
         <header className="header">
-          <a href="">ABOUT</a>
-          <a href="">PROJECTS</a>
-          <a href="">CONTACT</a>
+          <a className="fullsize-nav" href="">HOME</a>
+          <a className="home-icon" href=""></a>
+          <a className="fullsize-nav" href="">PORTFOLIO SITE</a>
+          <a className="fullsize-nav" href="">CONTACT</a>
+          <a className="contact-icon" href=""></a>
         </header>
         <div className="body">
-          <h1 className="title">Kylie Stewart</h1>
-          <div className="welcome">
-            <div className="welcome-div">
-              <p>More about me</p></div>
-            <div className="welcome-div">
-              <p>More about me</p></div>
-            <div className="welcome-div">
-              <p>More about me</p></div>
+          <h1 className="reveal-text">Kylie Stewart</h1>
+          <div className="aboutme">
+            <p></p>
           </div>
-
           <div className="welcome">
+            <div className="welcome-div">
+              <h2>Should I Hike Today?</h2>
+              <p>More about me</p></div>
+            <div className="welcome-div">
+              <h2>Locus: News Aggregator</h2>
+              <p>More about me</p></div>
+            <div className="welcome-div">
+              <p>More about me</p></div>
             <div className="welcome-div">
               <p>More about me</p></div>
             <div className="welcome-div">
@@ -32,7 +36,7 @@ class App extends Component {
           <div className="space"></div>
         </div>
         <footer className="footer">
-          Made with ❤️ in Phoenix. - view source
+          Made with ❤️ in Phoenix. → <a style={{ paddingLeft: '3px' }} href="https://github.com/kale-stew/mobile-portfolio" alt="checkout the git">view source</a>
         </footer>
       </div>
     );
