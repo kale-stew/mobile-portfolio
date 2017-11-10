086835da8eb7fd6af95a852caf7f0577ca26f6d8
Kylie Stewart <kylie.stewart@me.com>
2017-11-10 11:31:58 -0700

    Next step: Header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { fadeIn } from 'react-animations';
 import Radium, { StyleRoot } from 'radium';
+import { ContactCardIcon, PhoneIcon, MailIcon } from './icons';
 import './styles/Contact.css';
 
 const styles = {
@@ -15,7 +16,23 @@ const Contact = () => {
         <div className="body">
             <StyleRoot>
                 <div className="contact-container" style={styles.fadeIn}>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
+                    <div>
+                        <h3>I am available for freelance work</h3>
+                    </div>
+                    <div className="email-phone-wrap">
+                        <div className="email-phone-row">
+                            <MailIcon width="50px"/>
+                            <p style={{ marginLeft: '14px' }}>kylie.stewart@me.com</p>
+                        </div>
+                        <div className="email-phone-row">
+                            <PhoneIcon width="50px"/>
+                            <p style={{ marginLeft: '14px' }}>(202) 713-8956</p>    
+                        </div>
+                    </div>
+                    <div className="github-linkedin-wrap">
+                        <ContactCardIcon height="70px" width="70px"/>
+                    </div>
+                    <button style={{ marginTop: '20px' }}>Download my Resume</button>
                 </div>
             </StyleRoot>
             <div className="space"></div>
